4657e76363cb212a0dc69424072644fc37d7634f
AWiebe2021 <85322039+AWiebe2021@users.noreply.github.com>
2021-09-24 18:23:27 -0700

    added edit sheets

--- controllers/api/customer-routes.js
+++ controllers/api/customer-routes.js
@@ -52,4 +52,32 @@ router.delete('/:id', withAuth, (req, res) => {
     });
 });
 
+router.put('/:id', withAuth, (req, res) => {
+  Customer.update(
+    {
+      customer_name: req.body.customer_name,
+      customer_address: req.body. customer_address,
+      customer_city: req.body.customer_city,
+      customer_state: req.body.customer_state,
+      customer_zip: req.body.customer_zip
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  )
+    .then(dbCustomerData => {
+      if (!dbCustomerData) {
+        res.status(404).json({ message: 'No Customer found with this id' });
+        return;
+      }
+      res.json(dbCustomerData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
---
+++ controllers/edit-routes.js
@@ -0,0 +1,106 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Project, User, Customer, Process, Department } = require('../models');
+const withAuth = require('../utils/auth');
+
+// get all projects for editor
+router.get('/project', withAuth, (req, res) => {
+  console.log(req.session);
+  console.log('=====================');
+  Project.findAll({
+    attributes: [
+      'id',
+      'title',
+      'created_at',
+      'shipping_address',
+      'shipping_city',
+      'shipping_state',
+      'shipping_zip',
+      'customer_id',
+      'process_id'
+    ],
+    include:
+      {
+        model: Customer,
+        attributes: ['customer_name']
+      }
+    
+  })
+    .then(dbProjectData => {
+      const projects = dbProjectData.map(project => project.get({ plain: true }));
+      res.render('editProject', { projects, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/user', (req, res) => {
+  User.findAll({
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => {
+      const users = dbUserData.map(user => user.get({ plain: true }));
+      res.render('editUser', { users, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/customer', (req, res) => {
+  Customer.findAll({
+    attributes: [
+      'id',
+      'customer_name',
+      'customer_address',
+      'customer_city',
+      'customer_state',
+      'customer_zip'
+    ]
+  })
+  .then(dbCustomerData => {
+    const customers = dbCustomerData.map(customer => customer.get({ plain: true }));
+    res.render('editCustomer', { customers, loggedIn: true });
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/process', (req, res) => {
+  Process.findAll({
+    attributes:  ['id', 'processName','step','dept_id']
+  })
+  .then(dbProcessData => {
+    const processes = dbProcessData.map(process => process.get({ plain: true }));
+    res.render('editProcess', { processes, loggedIn: true });
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/department', (req, res) => {
+  console.log('======================');
+  Department.findAll({
+    attributes: [
+      'id',
+      'name'
+    ],
+  })
+  .then(dbDepartmentData => {
+    const departments = dbDepartmentData.map(department => department.get({ plain: true }));
+    res.render('editDepartment', { departments, loggedIn: true });
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
