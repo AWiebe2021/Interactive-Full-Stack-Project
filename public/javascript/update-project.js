a34f9129019053040f0174f5a7a6c2120eab36d8
AWiebe2021 <85322039+AWiebe2021@users.noreply.github.com>
2021-09-26 13:09:13 -0700

    fix projects

--- public/javascript/update-project.js
+++ public/javascript/update-project.js
@@ -2,10 +2,10 @@ async function updateProject(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="title"]').value;
-  const shipping_address = document.querySelector('input[name="project_address"]').value;
-  const shipping_city = document.querySelector('input[name="project_city"]').value;
-  const shipping_state = document.querySelector('input[name="project_state"]').value;
-  const shipping_zip = document.querySelector('input[name="project_zip"]').value;
+  const shipping_address = document.querySelector('input[name="shipping_address"]').value;
+  const shipping_city = document.querySelector('input[name="shipping_city"]').value;
+  const shipping_state = document.querySelector('input[name="shipping_state"]').value;
+  const shipping_zip = document.querySelector('input[name="shipping_zip"]').value;
   const project_id = document.querySelector('input[name="project_id"]').value;
   const customer_id = document.querySelector('input[name="customer_id"]').value;
   const process_id = document.querySelector('input[name="process_id"]').value;
